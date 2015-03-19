c06804cde6ebb510fa38725e3a57df81c173fb17
BrainDamaged84 <haris_mo@hotmail.com>
2015-03-19 16:35:10 +0100

    prvi commit

---
+++ MVCTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTest2.Startup))]
+namespace MVCTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
